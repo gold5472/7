f591377060a818136d155ade597fd5b41df56b20
gold5472 <gold5471s@gmail.com>
2016-12-17 17:20:49 +0000

    Create game_manager.js

---
+++ js/game_manager.js
@@ -0,0 +1,35 @@
+function GameManager(sizeofcards, Actuator, Dice)
+this.launchgame = true // Start the game
+this.sizeofcards = 33% // Size of the 3s
+this.won = false
+this.over = false
+this.state = state.normal
+this.action = true // Makes game function
+this.playervalue === 0
+this.otherplayervalue === 0
+self.data = true
+
+// Chose either Cube or Octahedron?
+GameManager.protoype.chooseDice = function {} {
+if (this.over = false) {
+var chosendice = Math.random() < 0.85 ? 6 : 8;
+var chosedice = value;
+
+}
+};
+
+// Clear the state when the game is over
+if (this.over = true) {
+this.clear = true;
+} else {
+this.state = state.normal
+
+}
+};
+
+if (this.clear = true) return; // Don't do anything if the game is over
+
+// The Winner
+if (this.playervalue === 7) this.won = true;
+var (this.action = false)
+
