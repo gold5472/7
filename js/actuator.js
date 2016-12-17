e213e9f4f0e2215075e6ce27123d3f729edb8dfb
gold5472 <gold5471s@gmail.com>
2016-12-17 17:23:42 +0000

    Create actuator.js

---
+++ js/actuator.js
@@ -0,0 +1,36 @@
+function Actuator() {
+this.action     = document.querySelector(".action");
+this.state      = document.querySelector(".state");
+
+}
+
+Actuator.prototype.actuate = function (Dice, metadata) { // Connects to the dice and metadata
+var self = this;
+
+}
+});
+
+// Stops the game when it is finished
+if (metadata = this.over) { 
+self.data = false;
+} else if (metadata = this.won) {
+self.data = true;
+  
+};
+  
+// Starts the Octahedron dice
+Actuator.prototype.actuate function self.dice { 
+  if dicechosen = 8;
+  var Math.random() < 0.125 ? 1 : 2 : 3 : -1 : -2 : -3 : 7/7 : •; 
+  this.rolled = value;
+    
+  }
+
+// Starts the Cube dice
+Actuator.prototype.actuate function self.dice { 
+  if dicechosen = 6;
+  var Math.random() < 0.166 ? 5 : -5 : Roll : Miss : 0 : 777;
+  this.rolled = value;
+  
+  }
+  
